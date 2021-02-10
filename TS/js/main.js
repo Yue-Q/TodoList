453ccce82abec33859b80ddf3e42b18184ae7e11
Esther Qiu <qiu00057@umn.edu>
2021-02-10 15:12:22 -0600

    converted js file

---
+++ TS/js/main.js
@@ -0,0 +1,21 @@
+var a1;
+a1 = "5";
+var a;
+a = 8;
+a = "hello";
+var num;
+var string;
+var bl;
+var u;
+var n;
+var b;
+var Arr = [1, 2, 3];
+var Arr1 = [1, 2, 3];
+var strArr = ["a", "b", "c"];
+function foo(name, age) {
+    return {
+        name: name,
+        age: age !== null && age !== void 0 ? age : -1
+    };
+}
+console.log(foo("Esther"));
