00f59013b5a051aac4341a080e16226ba546e51b
Esther Qiu <qiu00057@umn.edu>
2021-02-12 15:13:54 -0600

    react-router

--- react-class-component/src/App.js
+++ react-class-component/src/App.js
@@ -6,24 +6,34 @@ import WithTodosData from "./renderProps/withTodosData";
 import ColorfulTodo from "./components/TODO/colorfulTODO";
 import TODO from "./components/TODO/TODO";
 import Counter from "./components/counter/counter";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { MyRoute } from "./MyRouter/myRouter";
 function App() {
   return (
-    <Layout>
-      {/* <TodoCounts />
-      <TodoList /> */}
-      <Counter />
-      <WithTodosData render={(todos) => <TodoCounts count={todos.length} />} />
-      {/* <WithTodosData render={(todos, addTodo, removeTodo)=><TodoList todos={todos} addTodo={addTodo} removeTodo={removeTodo} />} /> */}
-      <WithTodosData>
-        {(todos, addTodo, removeTodo) => (
-          <TodoList todos={todos} addTodo={addTodo} removeTodo={removeTodo}>
-            {(todos, removeTodo) => todos?.map((item) => <TODO item={item} removeTodo={removeTodo} />)}
-          </TodoList>
-        )}
-      </WithTodosData>
-    </Layout>
+    <Router>
+      <Layout>
+        <MyRoute exact path="/about" render={() => <h1>About</h1>} />
+        <MyRoute exact path="/count">
+          <Counter />
+        </MyRoute>
+        <MyRoute exact path="/todoapp">
+          <WithTodosData render={(todos) => <TodoCounts count={todos.length} />} />
+          {/* <WithTodosData render={(todos, addTodo, removeTodo)=><TodoList todos={todos} addTodo={addTodo} removeTodo={removeTodo} />} /> */}
+          <WithTodosData>
+            {(todos, addTodo, removeTodo) => (
+              <TodoList todos={todos} addTodo={addTodo} removeTodo={removeTodo}>
+                {(todos, removeTodo) => todos?.map((item) => <TODO item={item} removeTodo={removeTodo} />)}
+              </TodoList>
+            )}
+          </WithTodosData>
+        </MyRoute>
+        <MyRoute exact path="/" component={Home} />
+      </Layout>
+    </Router>
   );
 }
 
 export default App;
+
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
---
+++ react-class-component/src/MyRouter/myRouter.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const routeInstance = [];
+
+const pathMatch = (path, exact) => {
+  const locationPath = window.location.pathname;
+  if (exact) {
+    return locationPath === path;
+  }
+  return locationPath.includes(path);
+};
+
+export class MyRoute extends React.Component {
+  render() {
+    const { path, children, exact = false, component, render } = this.props; //default value is false
+    console.log(exact);
+    console.log(this);
+    console.log(routeInstance);
+    if (pathMatch(path, exact)) {
+      if (component) {
+        return React.createElement(component, {}, null);
+      }
+      if (render) {
+        return render();
+      }
+      return children;
+    }
+    return null;
+  }
+  componentDidMount() {
+    routeInstance.push(this);
+    window.addEventListener("popstate", () => {
+      this.forceUpdate();
+    });
+  }
+}
+
+export class MyLink extends React.Component {
+  handleClick = (e) => {
+    e.preventDefault();
+    window.history.pushState({}, "", this.props.to); //update the url without refreshing the page
+    routeInstance.forEach((instance) => {
+      instance.forceUpdate();
+    });
+  };
+  render() {
+    const { to, children } = this.props;
+    return (
+      <a href={to} onClick={this.handleClick}>
+        {children}
+      </a>
+    );
+  }
+}
